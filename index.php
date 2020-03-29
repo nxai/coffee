0466cd34ed028d823afc039c7ea2ee8476a85a84
lao2dev <57119847+nxai@users.noreply.github.com>
2020-03-29 20:23:17 +0700

    Responsive by psd
    Responsive by psd

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Astounding</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   
+</head>
+<body>
+    <header>
+    <div class="hamburger" onclick="myFunction(this)">
+  <div class="bar1"></div>
+  <div class="bar2"></div>
+  <div class="bar3"></div>
+</div>
+
+   <div class="container">
+   <nav>
+    <div class="nav-grid">
+    <div class="logo">
+        <h1>Astounding</h1>
+        <span>Free PSD Website Template</span>
+        </div>
+        <!-- logo -->
+        <ul class="menu">
+        
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Styledemo</a></li>
+        <li><a href="#">Full width</a></li>
+        <li><a href="#">Dropdown</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Gallery</a></li>
+        </ul>
+        <!-- menu -->
+    </div>
+    </nav>
+   </div>
+   <!-- container -->
+    </header>
+    <!-- header -->
+    <section class="content">
+       <div class="container">
+       <div class="content-heading">
+            <h1>A Massive Title That Uses Loads Of Room.</h1>
+        </div>
+        <!-- content-heading -->
+    <div class="content-services">
+            <div class="content-services-grid">
+                <div class="conten-services-item">
+                <i class="fa fa-camera-retro fa-lg" aria-hidden="true"></i> 
+                    <h2>Service Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A amet sint nihil corporis non aspernatur molestiae maxime mollitia excepturi facilis, delectus beatae voluptatem necessitatibus repellendus? Eos tenetur quae corporis libero.</p>
+
+                </div>
+                <div class="conten-services-item">
+                <i class="fa fa-bars"></i> 
+                    <h2>Service Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A amet sint nihil corporis non aspernatur molestiae maxime mollitia excepturi facilis, delectus beatae voluptatem necessitatibus repellendus? Eos tenetur quae corporis libero.</p>
+
+                </div>
+                <div class="conten-services-item">
+                <i class="fa fa-heart"></i> 
+                    <h2>Service Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A amet sint nihil corporis non aspernatur molestiae maxime mollitia excepturi facilis, delectus beatae voluptatem necessitatibus repellendus? Eos tenetur quae corporis libero.</p>
+
+                </div>
+                <div class="conten-services-item">
+                <i class="fa fa-cloud"></i> 
+                    <h2>Service Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A amet sint nihil corporis non aspernatur molestiae maxime mollitia excepturi facilis, delectus beatae voluptatem necessitatibus repellendus? Eos tenetur quae corporis libero.</p>
+
+                </div>
+            </div>
+            <!-- content-services-grid -->
+    
+    </div>
+    <!-- content-services -->
+            <div class="content-meet">
+            <h1>Meet our team</h1>
+            </div>
+            <!-- content-meet -->
+
+                <div class="content-teams">
+                    <div class="content-teams-grid">
+                        <div class="content-teams-item">
+                            <img src="https://via.placeholder.com/225" alt="" srcset="">
+                            <h2>Your Name</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut eveniet doloribus eligendi labore rem dolore assumenda laudantium molestias et corrupti debitis necessitatibus, commodi natus. Cumque sapiente labore id culpa vero.</p>
+                           <div class="cr"> <a href="#">view profile>></a></div>
+                        </div>
+                        <div class="content-teams-item">
+                            <img src="https://via.placeholder.com/225" alt="" srcset="">
+                            <h2>Your Name</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut eveniet doloribus eligendi labore rem dolore assumenda laudantium molestias et corrupti debitis necessitatibus, commodi natus. Cumque sapiente labore id culpa vero.</p>
+                            <div class="cr"> <a href="#">view profile>></a></div>
+                        </div>
+                        <div class="content-teams-item">
+                            <img src="https://via.placeholder.com/225" alt="" srcset="">
+                            <h2>Your Name</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut eveniet doloribus eligendi labore rem dolore assumenda laudantium molestias et corrupti debitis necessitatibus, commodi natus. Cumque sapiente labore id culpa vero.</p>
+                            <div class="cr"> <a href="#">view profile>></a></div>
+                        </div>
+                        <div class="content-teams-item">
+                            <img src="https://via.placeholder.com/225" alt="" srcset="">
+                            <h2>Your Name</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut eveniet doloribus eligendi labore rem dolore assumenda laudantium molestias et corrupti debitis necessitatibus, commodi natus. Cumque sapiente labore id culpa vero.</p>
+                            <div class="cr"> <a href="#">view profile>></a></div>
+                        </div>
+                    </div>
+                    <!-- content-teams-grid -->
+                </div>
+                <!-- content-teams -->
+       </div>
+       <!-- container -->
+    </section>
+    <!-- content -->
+    <footer>
+    <div class="container">
+   <div class="footer-grid">
+            <div class="footer-item">
+            <h3> From the Blog</h3>
+            <h4>Post title</h4>
+            <span class="admin"><a href="#">admin</a>, Domainame.com</span><br>
+            <span class="date">friday,6th april 2020</span>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, eveniet! Officiis, atque odit, ratione repellendus dolorum aut, natus eum assumenda minus ullam deleniti blanditiis eligendi debitis nam? Nam, sint dolorem!</p>
+            <a href="#">Read more >></a>
+            </div>
+
+            <div class="footer-item">
+            <h3> Quick Links</h3>
+           <ul>
+           <li> <a href=""> >> lorem</a> </li>
+           <li> <a href=""> >> lorem</a> </li>
+           <li> <a href=""> >> lorem</a> </li>
+           <li> <a href=""> >> lorem</a> </li>
+           <li> <a href=""> >> lorem</a> </li>
+           <li> <a href=""> >> lorem</a> </li>
+           <li> <a href=""> >> lorem</a> </li>
+           <li> <a href=""> >> lorem</a> </li>
+           </ul>
+            </div>
+            <div class="footer-item">
+            <h3> Lastest Tweets</h3>
+         <p> <a href="#">@namehee</a> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+         <p> <a href="#">@namehee</a> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+        
+            </div>
+            <div class="footer-item">
+                <h3>Contact Us</h3>
+            <form action="">
+            <input type="text" name="username" id="username" placeholder="Fullname">
+            <input type="email" name="email" id="email" placeholder="email">
+            <input type="text" name="subject" id="" placeholder="subject">
+            <textarea name="massage" id="" cols="30" rows="10" placeholder="massage"></textarea>
+            <input type="submit" value="submit">
+            </form>
+            </div>
+
+   </div>
+   </div>
+    
+    </footer>
+    <!-- footer -->
+   
+   <div class="copyright">
+   
+   <div class="container">
+   <div class="copyright-grid">
+   <div class="copytext">
+   <p>Copyright &copy; 2020 Domain Name - All Right Reserved</p>
+   </div>
+   <div class="credit">
+   <p>Template by Somxai</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   <script src="js/jquery.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
